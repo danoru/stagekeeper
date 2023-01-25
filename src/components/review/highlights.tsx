7fb325271da484c57dc332171970cb9c461a5d13
Daniel Mendez <awilddanoru@gmail.com>
2023-01-25 08:47:37 -0800

    Refactored code to make new const to replace duplicate lines of code

--- src/components/review/highlights.tsx
+++ src/components/review/highlights.tsx
@@ -9,36 +9,36 @@ interface Props {
 function Highlights(props: Props) {
   const { highlights } = props;
   const musicalCount = highlights.length;
+  const filteredHighlights = highlights.filter(
+    (highlight: MUSICAL_LIST_TYPE) => highlight.groupAttended
+  );
 
-  const totalDuration = highlights
-    .filter((highlight: MUSICAL_LIST_TYPE) => highlight.groupAttended)
-    .reduce((accumulator: number, highlight: MUSICAL_LIST_TYPE): number => {
+  const totalDuration = filteredHighlights.reduce(
+    (accumulator: number, highlight: MUSICAL_LIST_TYPE): number => {
       return accumulator + highlight.duration;
-    }, 0);
+    },
+    0
+  );
 
   const locationStorage: Record<string, number> = {};
   let locationCount = 0;
 
-  highlights
-    .filter((highlight: MUSICAL_LIST_TYPE) => highlight.groupAttended)
-    .forEach((highlight: MUSICAL_LIST_TYPE) => {
-      if (!locationStorage[highlight.location]) {
-        locationStorage[highlight.location] = 1;
-        locationCount++;
-      }
-    });
+  filteredHighlights.forEach((highlight: MUSICAL_LIST_TYPE) => {
+    if (!locationStorage[highlight.location]) {
+      locationStorage[highlight.location] = 1;
+      locationCount++;
+    }
+  });
 
   const stageStorage: Record<string, number> = {};
   let stageCount = 0;
 
-  highlights
-    .filter((highlight: MUSICAL_LIST_TYPE) => highlight.groupAttended)
-    .forEach((highlight: MUSICAL_LIST_TYPE) => {
-      if (!stageStorage[highlight.playhouse]) {
-        stageStorage[highlight.playhouse] = 1;
-        stageCount++;
-      }
-    });
+  filteredHighlights.forEach((highlight: MUSICAL_LIST_TYPE) => {
+    if (!stageStorage[highlight.playhouse]) {
+      stageStorage[highlight.playhouse] = 1;
+      stageCount++;
+    }
+  });
 
   return (
     <div className={styles.stuff}>

7061f3a6cd22591850c51e40b67f9081a3472006
Daniel Mendez <awilddanoru@gmail.com>
2023-01-24 16:54:58 -0800

    Adds types for 2022 data

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface MUSICAL_LIST_TYPE {
+  date: string;
+  duration: number;
+  genre: string;
+  groupAttended: boolean;
+  image: string;
+  location: string;
+  playhouse: string;
+  premiere: string;
+  title: string;
+}

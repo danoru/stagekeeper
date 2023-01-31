a4e531dc0305072aef0d84c2ba21a1d33f2d4009
Daniel Mendez <awilddanoru@gmail.com>
2023-01-31 13:05:54 -0800

    Adds error pages to redirect to home

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,22 +1,22 @@
 import { Fragment } from "react";
 import Head from "next/head";
 
-function UpcomingPage() {
+function ErrorPage() {
   return (
     <Fragment>
       <Head>
-        <title>StageKeeper: Your Upcoming Events</title>
-        <meta name="description" content="See what your new year looks like!" />
+        <title>StageKeeper: 404</title>
       </Head>
       <blockquote cite="https://genius.com/Jonathan-groff-lost-in-the-woods-lyrics">
         Now I turn around and find I am lost in the woods, <br />
         North is south, right is left, when you're gone.
       </blockquote>
       <p>
-        Are you lost? Let's get you back on <a href="/review">track</a>.
+        Are you lost? Let's get you back on <a href="/">track</a>.
       </p>
+      <p>404 | This page could not be found.</p>
     </Fragment>
   );
 }
 
-export default UpcomingPage;
+export default ErrorPage;

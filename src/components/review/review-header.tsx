eec935bab2187797fadb8b29bb103c596c2f269a
Daniel Mendez <awilddanoru@gmail.com>
2023-02-28 09:38:42 -0800

    Updates review header element to allow for navigation

--- src/components/review/review-header.tsx
+++ src/components/review/review-header.tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-function ReviewHeader(context: any) {
+function ReviewHeader() {
   const router = useRouter();
   const year = router.query.year;
   const userId = router.query.userId;
+
+  const previousYear = Number(year) - 1;
+  const prevYearURL = "/users/musicalsandmayhem/review/" + [previousYear];
+
+  const nextYear = Number(year) + 1;
+  const nextYearURL = "/users/musicalsandmayhem/review/" + [nextYear];
+
+  function musicalScopeHandler() {
+    if (userId === "all") {
+      return router.push("/users/musicalsandmayhem/review/" + [year]);
+    } else {
+      return router.push("/users/all/review/" + [year]);
+    }
+  }
 
   const humanReadableUsername = () => {
     if (userId === "musicalsandmayhem") {
@@ -25,8 +41,16 @@ function ReviewHeader(context: any) {
 
   return (
     <div>
-      <h1>{humanReadableUsername()}</h1>
-      <h2>{humanReadableYear()}</h2>
+      <h1 onClick={musicalScopeHandler}>{humanReadableUsername()}</h1>
+      <h2>
+        <a href={prevYearURL}>
+          <ArrowBackIosIcon />
+        </a>
+        {humanReadableYear()}
+        <a href={nextYearURL}>
+          <ArrowForwardIosIcon />
+        </a>
+      </h2>
     </div>
   );
 }

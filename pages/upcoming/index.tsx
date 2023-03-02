aea708af83ec117259a58af088a104c6ec080cc8
Daniel Mendez <awilddanoru@gmail.com>
2023-03-02 14:59:37 -0800

    Adds functional calendar to upcoming section

--- pages/upcoming/index.tsx
+++ pages/upcoming/index.tsx
@@ -1,11 +1,15 @@
 import { Fragment, useState } from "react";
 
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
 import Head from "next/head";
+
+import styles from "../../src/styles/upcoming.module.css";
+import "react-calendar/dist/Calendar.css";
 
 function UpcomingPage() {
   const [value, onChange] = useState(new Date());
+  const minimumDate = new Date("01-01-2023");
+  const maximumDate = new Date("12-31-2024");
 
   return (
     <Fragment>
@@ -14,7 +18,15 @@ function UpcomingPage() {
         <meta name="description" content="See what your new year looks like!" />
       </Head>
       <h1>This page is currently in development.</h1>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar
+        calendarType="US"
+        className={styles.calendar}
+        maxDate={maximumDate}
+        minDate={minimumDate}
+        minDetail="year"
+        onChange={onChange}
+        value={value}
+      />
     </Fragment>
   );
 }

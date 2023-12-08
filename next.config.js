76d51b4071a9d4a23225e4de517130975b349a09
Daniel Mendez <awilddanoru@gmail.com>
2023-12-08 10:10:26 -0800

    Update data for year in review

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.imgur.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "imgur.com",
+        pathname: "**",
+      },
+    ],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
